9775246486ddf39ac97bb295937f98f21fcf23ed
Nishu Lodhi <119037232+nishulodhi174@users.noreply.github.com>
2023-10-12 00:10:36 +0530

    Time: 0 ms (100%), Space: 41 MB (36.79%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,26 +1,17 @@
-// class Solution {
-//     public int removeElement(int[] nums, int val) {
-//         ArrayList<Integer> list = new ArrayList<>();
-//         for(int i=0;i<nums.length;i++){
-//             if(nums[i] < val || nums[i] > val){
-//                 list.add(nums[i]);
-//             }
-//         }
-//         return list.size();
-//     }
-// }
-
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int i = 0;
-        for (int j = 0; j < nums.length; j++) {
-            if (nums[j] != val) {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                i++;
+        
+        int newLength = 0 ; 
+
+         for (int i = 0; i < nums.length; i++) {
+           
+            if (nums[i] != val) {
+                nums[newLength] = nums[i];
+                newLength++;
             }
         }
-        return i;
+
+        return newLength;
+        
     }
 }
