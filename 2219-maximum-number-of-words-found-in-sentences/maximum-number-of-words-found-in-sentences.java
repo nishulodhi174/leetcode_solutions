5c1d9e9268097d67cb385930a2cf71e3fbfe9945
Nishu Lodhi <119037232+nishulodhi174@users.noreply.github.com>
2024-01-25 19:44:45 +0530

    Time: 4 ms (34.95%) | Memory: 41.9 MB (93.32%) - LeetSync

---
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+        int count = 0;
+        int max = 0;
+        for(int i=0 ; i < sentences.length ; i++){
+            for(int j=0 ; j < sentences[i].length() ; j++){
+                if(sentences[i].charAt(j) == ' '){
+                    count++;
+                }
+            }
+            if(max < count){
+                max = count;
+            }
+            count = 0;
+        }
+        return max+1;
+    }
+}
