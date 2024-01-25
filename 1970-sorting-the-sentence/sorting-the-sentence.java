35f117e7cf9c88b059e68624a5f9b1ec770dc713
Nishu Lodhi <119037232+nishulodhi174@users.noreply.github.com>
2024-01-25 11:16:34 +0530

    Time: 1 ms (59.44%) | Memory: 41.7 MB (16.50%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.java
+++ 1970-sorting-the-sentence/sorting-the-sentence.java
@@ -1,17 +1,21 @@
-class Solution{
-    public static String sortSentence(String s) {
-        String[] str = s.split(" ");
-        String[] res = new String[str.length];
-        StringBuilder sb = new StringBuilder();
-        
-        int i = 0;                          
-        for (String elem : str) {
-            i = (int) (elem.charAt(elem.length() - 1) - '0');
-            res[i - 1] = elem.substring(0, elem.length() - 1);
+class Solution {
+    public String sortSentence(String s) {
+        String arr[]=s.split(" ");
+        String ans[]=new String[arr.length];
+
+        for(int i=0;i<arr.length;i++){
+            int idx=Character.getNumericValue(arr[i].charAt(arr[i].length()-1));
+            //System.out.print(arr[i]+" "+idx);
+            idx--;
+            ans[idx]=arr[i].substring(0,arr[i].length()-1)+" ";
         }
-        for (i = 0; i < res.length - 1; i++)
-            sb.append(res[i]).append(" ");
-        sb.append(res[i]);
-        return sb.toString();
-    }  
+        //System.out.println(arr.length);
+        String r="";
+        for(int i=0;i<ans.length;i++){
+            //System.out.print(ans[i]+" ");
+            r+=ans[i];
+        }
+        r=r.trim();
+        return r;
+    }
 }
