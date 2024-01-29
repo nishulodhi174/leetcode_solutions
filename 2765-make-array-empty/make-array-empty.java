3670537ffacf4320de65d435feac7276c06ef3f9
Nishu Lodhi <119037232+nishulodhi174@users.noreply.github.com>
2024-01-29 20:20:26 +0530

    Time: 56 ms (53.95%) | Memory: 61 MB (52.63%) - LeetSync

---
+++ 2765-make-array-empty/make-array-empty.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long countOperationsToEmptyArray(int[] nums) {
+        HashMap<Integer, Integer> pos = new HashMap<>();
+        long n= nums.length, res = n;
+        for(int i = 0;i<n;i++){
+            pos.put(nums[i], i);
+        }
+        Arrays.sort(nums);
+        for(int i = 1;i<n;i++){
+            if(pos.get(nums[i]) < pos.get(nums[i - 1])){
+                res += n-i;
+            }
+        }
+        return res;
+    }
+}
