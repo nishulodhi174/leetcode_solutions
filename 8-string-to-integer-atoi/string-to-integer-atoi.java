8e1dafe6d2d48e07240b86d981ae31b6d5780aeb
Nishu Lodhi <119037232+nishulodhi174@users.noreply.github.com>
2024-02-29 11:58:34 +0530

    Time: 2 ms (44.09%) | Memory: 42 MB (95.33%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,58 +1,29 @@
 class Solution {
     public int myAtoi(String s) {
-        int si=0,l=s.length(),flag=Integer.MAX_VALUE/10;
-        boolean neg=false,range=false,sign=false,dig=false;
-        first:for(int i=0;i<l;i++){
-            char ch=s.charAt(i);
-            if((sign==true)&&(ch<48 || ch>57))
-                break;
-            if((ch<48 || ch>57)&&(ch!=43 && ch!=45 && ch!=32))
-                break; 
-            else if(ch>=48 && ch<=57){
-                dig=true;
-                if((si > flag) || (si == flag && ch > '7')){
-                    range=true;
-                    break;
-                }
-                int a=ch-'0';
-                si=si*10+a;
-            }
-            else{
-                switch(ch){
-                    case '-':
-                        if(dig==true)
-                            break first;
-                        else if(sign==false){
-                            sign=true;
-                            neg=true;
-                        }
-                        else
-                            return 0;
-                        break;
-                    case '+':
-                        if(dig==true)
-                            break first;
-                        else if(sign==false)
-                            sign=true;
-                        else 
-                            return 0;
-                        break;
-                    case ' ':
-                        if(dig==true)
-                            break first;
-                        break;
-                    default:
-                        break first;
-                }
-            }
+        s = s.trim();
+        if (s.isEmpty()) {
+            return 0;
         }
-        if(neg==false && range==false)
-            return si;
-        else if(neg==false && range==true)
-            return Integer.MAX_VALUE;
-        else if(neg==true && range==false)
-            return si*(-1);
-        else
-            return Integer.MIN_VALUE;
+
+        int ans = 0, i = 0;
+        boolean neg = s.charAt(0) == '-';
+        boolean pos = s.charAt(0) == '+';
+
+        if (neg || pos) {
+            i++;
+        }
+
+        while (i < s.length() && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+
+            if (ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10 && digit > Integer.MAX_VALUE % 10)) {
+                return neg ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+
+            ans = ans * 10 + digit;
+            i++;
+        }
+
+        return neg ? -ans : ans;
     }
 }
