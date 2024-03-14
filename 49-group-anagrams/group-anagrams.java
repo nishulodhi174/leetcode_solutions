bd7ec7534040e0bd7bc57e9fa2f7516fa6da518f
Nishu Lodhi <119037232+nishulodhi174@users.noreply.github.com>
2024-03-14 16:51:31 +0530

    Time: 6 ms (96.38%) | Memory: 47.5 MB (68.07%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,20 +1,36 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> map = new HashMap<>();
-
-        for(String str : strs){
-            char ch[] = str.toCharArray();
-            Arrays.sort(ch);
-            String newStr = new String(ch);
-
-            if(map.containsKey(newStr)){
-                map.get(newStr).add(str);
-            }else{
-                List<String> list = new ArrayList<>();
-                list.add(str);
-                map.put(newStr,list);
-            }            
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
         }
-        return new ArrayList<>(map.values());
+
+        List<List<String>> result = new ArrayList<>();
+
+        Map<String, List<String>> groups = new HashMap<>();
+
+        for (String word : strs) {
+            String key = generateKey(word);
+
+            List<String> words = groups.getOrDefault(key, new ArrayList());
+            words.add(word);
+            groups.put(key, words);
+        }
+
+        for (String groupKey : groups.keySet()) {
+            result.add(groups.get(groupKey));
+        }
+
+        return result;
+    }
+
+    private String generateKey(String word) {
+        char[] chars = new char[26];
+
+        for (Character c : word.toCharArray()) {
+            int index = c - 'a';
+            chars[index]++;
+        }
+
+        return String.valueOf(chars);
     }
 }
