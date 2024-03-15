b7aaefb5904635d9469992e257f155e54ab9406b
Nishu Lodhi <119037232+nishulodhi174@users.noreply.github.com>
2024-03-15 18:15:37 +0530

    Time: 16 ms (31.09%) | Memory: 48.1 MB (72.96%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,22 +1,30 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer,Integer>map=new HashMap<>();
-        for(int i:nums)
-        {
-            if(map.containsKey(i))
-            {
-                map.put(i,map.get(i)+1);
+        TreeMap<Integer, Set<Integer>> counts = new TreeMap<Integer, Set<Integer>>();
+        Map<Integer, Integer> items = new HashMap<Integer, Integer>();
+        int result[] = new int[k];
+        
+        for(int i = 0; i < nums.length; i++){
+            if (!items.containsKey(nums[i])){
+                items.put(nums[i], 0);
             }
-            else{
-                map.put(i,1);
-            }
+            items.put(nums[i], items.get(nums[i]) + 1);
         }
-        List<Integer>list=new ArrayList<>(map.keySet());
-        list.sort((a, b) -> map.get(b) - map.get(a));
-        int res[] = new int[k];
-        for (int i = 0; i < k; ++i)
-            res[i] = list.get(i);
-        return res;
-
+        for(Integer num : items.keySet()){
+            Integer temp = items.get(num);
+            if(!counts.containsKey(temp)){
+                counts.put(temp, new HashSet<Integer>());
+            }
+            counts.get(temp).add(num);
+        }
+        int count = 0;
+        while(count < k){
+            for (Integer element : counts.get(counts.lastKey())){
+                result[count] = element;
+                count++;
+            }
+            counts.pollLastEntry();
+        }
+        return result;
     }
 }
