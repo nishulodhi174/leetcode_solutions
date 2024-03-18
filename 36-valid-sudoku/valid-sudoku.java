81f43cbccb639c8be7117e0cc1cae57ca962aa06
Nishu Lodhi <119037232+nishulodhi174@users.noreply.github.com>
2024-03-18 15:26:42 +0530

    Time: 1 ms (100.00%) | Memory: 44.7 MB (37.75%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,12 +1,18 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set seen = new HashSet();
-        for (int i=0; i<9; ++i) {
-            for (int j=0; j<9; ++j) {
-                if (board[i][j] != '.') {
-                    String b = "(" + board[i][j] + ")";
-                    if (!seen.add(b + i) || !seen.add(j + b) || !seen.add(i/3 + b + j/3))
+        int[] rowMark = new int[9], colMark = new int[9], subboxMark = new int[9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (Character.isDigit(board[i][j])) {
+                    int num = board[i][j] - '0';
+                    int subboxIdx = 3 * (i / 3) + j / 3;
+                    int mask = 1 << num;
+                    if ((mask & rowMark[i]) != 0 || (mask & colMark[j]) != 0 || (mask & subboxMark[subboxIdx]) != 0) {
                         return false;
+                    }
+                    rowMark[i] |= mask;
+                    colMark[j] |= mask;
+                    subboxMark[subboxIdx] |= mask;
                 }
             }
         }
