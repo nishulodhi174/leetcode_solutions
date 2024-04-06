aba23af3026a3121a714a78f98fa4cdd951e7293
Nishu Lodhi <119037232+nishulodhi174@users.noreply.github.com>
2024-04-07 00:04:03 +0530

    Time: 21 ms (55.16%) | Memory: 45.7 MB (20.56%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,48 +1,33 @@
-
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        // Initialize pointers for the start and end of the string
-        int startPointer = 0;
-        int endPointer = s.length() - 1;
-
-        String result;
-
-        // Convert input string to character array for easier manipulation
-        char[] arr = s.toCharArray();
-        
-        // Counter for open parentheses
-        int openParenthesesCount = 0;
-
-        // First pass: mark excess closing parentheses with '*'
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == '(')
-                openParenthesesCount++;
-            else if (arr[i] == ')') {
-                if (openParenthesesCount == 0)
-                    arr[i] = '*'; // Mark excess closing parentheses
-                else
-                    openParenthesesCount--;
+        int opens = 0;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.length() ; i++) {
+            if(s.charAt(i) == '(') {
+                opens++;
+            } else {
+                if(opens > 0) {
+                    if(s.charAt(i) == ')')
+                        opens--;
+                } else {
+                    if(s.charAt(i) == ')')
+                        continue;
+                }
             }
+            sb.append(s.charAt(i));
         }
-
-        // Second pass: mark excess opening parentheses from the end
-        for (int i = arr.length - 1; i >= 0; i--) {
-            if (openParenthesesCount > 0 && arr[i] == '(') {
-                arr[i] = '*'; // Mark excess opening parentheses
-                openParenthesesCount--;
+        StringBuilder res = new StringBuilder();
+        if(opens > 0) {
+            for(int i = sb.length()-1; i >=0 ; i--)  {
+                if(sb.charAt(i) == '(' && opens > 0){
+                    opens--;
+                    continue;
+                } 
+                res.append(sb.charAt(i));
             }
+        } else {
+            return sb.toString();
         }
-        
-        // Filter out marked characters and store the result in the character array
-        int p = 0; // Pointer for updating the character array
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != '*')
-                arr[p++] = arr[i];
-        }
-
-        // Construct the result string from the filtered character array
-        result = new String(arr).substring(0, p);
-
-        return result;
+        return res.reverse().toString();
     }
 }
